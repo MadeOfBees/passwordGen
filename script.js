fc60b25cefe462330bd37e650d50f94e6dc3a482
Bee <xronsredstone@gmail.com>
2022-09-14 15:59:57 -0400

    upload starter page

---
+++ script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+var numOfNum
+
+function generate() {
+  numOfNum = prompt("Please enter your name:", "Number here");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
