17d0f19133019de38cd724de7e8b12e11304d969
Bee <xronsredstone@gmail.com>
2022-09-14 21:48:24 -0400

    maybe

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 // var generateBtn = document.querySelector("#generate");
 var first
 
+// Not really doing what I was hoping for....
 function generate() {
   first = prompt("How many digits?", "Number here");
-  console.log (first);
+  return [first];
 }
+if ((first > 8) || (first < 127) && (typeof first !== 'number' )){
+  console.log = "you picked a valid number";
+} else {
+  console.log = "not a valid number, please choose between 8 and 127 chars";
+}
+
 
 
 // // Write password to the #password input
