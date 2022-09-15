28835e5d1108f7e8e7839aef9ee7208724b24557
Bee <xronsredstone@gmail.com>
2022-09-15 16:34:16 -0400

    no reason to get excited

--- script.js
+++ script.js
@@ -3,30 +3,32 @@ var alphabetL = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var number = ["1","2","3","4","5","6","7","8","9","0"];
 var symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'","<", ",", ">",".","?","/"];
 var questions = ["How many digits?", "Do you want capital letters? (write true for yes or false", "Do you want numbers?", "Do you want numbers?"];
-var Answers = ["","","","",]
+var answers = ["","","","",]
 var generateBtn = document.getElementById('generate')
 generateBtn.addEventListener("click", writePassword);
 
 
 function writePassword() {
-  Answers[0] = prompt(questions[0]);
-  console.log (Answers[0])
-  if ((Answers[0] > 8) && (Answers[0] < 127)){
-    console.log("you picked a valid number"); 
+  answers[0] = prompt(questions[0]);
+  if ((answers[0] > 8) && (answers[0] < 127)){
     for (var i = 1; i < 3; i++){
-    Answers[i] = prompt(questions[i] + " (write true for yes or false for no)");
-    console.log(Answers)
-    return(Answers)}
+      answers[i] = prompt(questions[i] + " (write y for yes or n for no)");
+    return(answers)}
   } else {
     alert("You picked an ivalid number please choose between 8 and 127 chars")
   }
 }
 
-function askQuestions() {
-  console.log("yee")
-  for (var i = 1; i < 3; i++){
-    Answers[i] = prompt(questions[i] + " (write true for yes or false for no)");
-    console.log(Answers)
-    return[Answers]}
-    
-}
+// function generatePassword(){
+
+// }
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
