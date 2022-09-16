2290e59aebcdefaceb5220c978f6b9235c4bf7eb
Bee <xronsredstone@gmail.com>
2022-09-16 17:30:35 -0400

    finishing touches

--- script.js
+++ script.js
@@ -7,36 +7,33 @@ var number = ["1","2","3","4","5","6","7","8","9","0"];
 // "All" possible symbols in an array
 var symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'","<", ",", ">",".","?","/"];
 // All the questions we will ask the user
-var questions = ["How many digits?", "Do you want capital letters?", "Do you want numbers?", "Do you want symbols?"];
+var questions = ["How many digits?", "capital letters", "numbers", "symbols"];
 // Here's where we store all the user's answers
 var answers = ["","","","",];
 // connects the variable generate Btn to the spot on our HTML that we set the ID of generate (to a button) 
 var generateBtn = document.getElementById('generate');
-// looks for when the button's clicked and then executes the function to ask user for peramiters
 var possibleChar
+// looks for when the button's clicked and then executes the function to ask user for peramiters
 generateBtn.addEventListener("click", askQuestions);
 // And here's that function
-
-
 function askQuestions(){
   answers[0] = prompt(questions[0]);
-  if ((answers[0] > 8) && (answers[0] < 127)){
+  if ((answers[0] >= 8) && (answers[0] <= 128)){
     for (var i = 1; i < 4; i++){
-      answers[i] = confirm(questions[i]);}
+      answers[i] = confirm("Do you want " + (questions[i]) + "? (Click OK to confirm you would like " + (questions[i]) + " or click cancel if you'd like to continue without " + (questions[i]) + ".");}
   } else {
-    alert("You picked an ivalid number please choose between 8 and 127 chars")
+    alert("You picked an ivalid number please choose between 8 and 128 chars")
   }
   letterSoup(answers);
   answers = ["","","","",];
   return(answers);}
 // this function creats a list of possible characters based on our criteria from the quest
-
 function letterSoup(answers){possibleChar = (alphabetL);
 if (answers[1]){possibleChar = possibleChar.concat(alphabetU);}
 if (answers[2]){possibleChar = possibleChar.concat(number);}
 if (answers[3]){possibleChar = possibleChar.concat(symbols);}
 buildPassword(answers, possibleChar);}
-
+// this function takes all our building blocks from the previous steps and builds a random password based on our parameters from the first function
 function buildPassword(answers, possibleChar){
   var passwordBlocks = [""];
   for (var i = answers[0]; i > 0; i--){
@@ -45,7 +42,7 @@ function buildPassword(answers, possibleChar){
   }
   passwordBlocks.shift();
   password = passwordBlocks.join("")
-  console.log (password);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+  generateBtn.setAttribute("style", "background-color: rgb(92, 212, 76);");
 }
